5067acd340bf5a96e063ebc750f4f263681f282c
Premod1 <premodsuraweera1@gmail.com>
2022-08-29 23:27:35 +0530

    init

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+
+const router = createRouter({
+	history: createWebHistory(),
+	routes: [
+		{
+			path: '/',
+			component: Home
+		},
+		{
+			path: '/about',
+			component: () => import('../views/About.vue')
+		},
+		{
+			path: '/contact',
+			component: () => import('../views/Contact.vue')
+		}
+	],
+})
+
+export default router
---
+++ src/views/Contact.vue
@@ -0,0 +1,6 @@
+<template>
+	<main id="Contact">
+		<h1>Contact</h1>
+		<p>This is the home page</p>
+	</main>
+</template>
